6c9f2a57a337f14de14b4b2e851f6bfa9df7f9be
thenameisahalya <ahalyavl24@gmail.com>
2024-06-11 14:19:24 +0530

    project done

--- models/student.js
+++ models/student.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
         
-        "name":String,
-        "admNo": String,
-       " rollNo": String,
-        "dob": String,
-        "course":String,
-        "mob": String,
-        "email": String,
-        "address": String
+        "name":{type:String,required:true},
+        "admNo": {type:String,required:true},
+      
+        "dob": {type:String,required:true},
+        "course":{type:String,required:true},
+        "mob": {type:String,required:true},
+        "email": {type:String,required:true},
+        "address":{type:String,required:true}
     }
 )
 
